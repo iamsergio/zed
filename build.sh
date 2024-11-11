e697ba661a63074c72d87333581987d528152b7f
Sergio Martins <@>
2024-11-11 14:07:28 +0000

    macos: fix copying webrtc

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ cargo build --release && \
 cp ./target/release/zed $DATA_DIR/installation/bin/ || exit 1
 
 if [ -d target/release/WebRTC.framework ] ; then
-    cp -r target/release/WebRTC.framework $DATA_DIR/installation/bin/
+    cp -R target/release/WebRTC.framework $DATA_DIR/installation/bin/
 fi
 
